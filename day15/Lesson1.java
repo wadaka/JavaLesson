ced7472ec98912794102670ab53398c75630745f
wadaka <erou1227@yahoo.co.jp>
2022-04-07 14:37:56 +0900

    凶のやつ

--- day15/Lesson1.java
+++ day15/Lesson1.java
@@ -1,15 +1,30 @@
 import java.util.*;
 public class Lesson1{
 	public static void main(String[] args){
-
+		final int LINE_ROWS = 3;
+		final int LINE_COLUMNS = 3;
+		int[][] start= new int[LINE_ROWS][LINE_COLUMNS];
+		for(int i=0;i<start.length;i++){
+			for(int j=0;j<start[i].length;j++){
+				start[i][j] = new Random().nextInt(3)+1;
+				System.out.print(start[i][j]);
+			}
+			System.out.println();
+		}
+		int win = Same(start);
+		System.out.printf(win !=0 ? "%d line win!":"Lose...",win);
 	}
-	public static boolean isSame(int[] arr){
-		int first=arr[0]; 
+	public static int Same(int[][] arr){
+		int missLine = 0;
 		for(int i=0;i<arr.length;i++){
-			if(first != arr[i]){
-				return dalse;
+			int first = arr[i][0];
+			for(int j=1;j<arr[i].length;j++){
+				if(first != arr[i][j]){
+					missLine ++;
+					break;
+				}
 			}
 		}
-		return true;
+		return arr.length -missLine; 
 	}
 }
